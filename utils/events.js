c5c013a6c0a29a2828a6db18ca4e1980093d1073
FilipBukowiec <102471937+FilipBukowiec@users.noreply.github.com>
2024-05-13 15:46:17 +0200

    add murph

--- utils/events.js
+++ utils/events.js
@@ -6,7 +6,7 @@ import { countdown } from "./countdown.js";
 
 export const crossfitBytomThrowdown = `<div class="swiper-slide">
   <div class="event-bg">
-    <img id="bytomthd" src="./media/bytom.png" alt="" />
+    <img id="bytomthd" src="./media/murph.png" alt="" />
     <div class="countdown-wrapper">
       <div class="countdown-time"><span id="days"></span>Days</div>
       <div class="countdown-time">
--- utils/media.js
+++ utils/media.js
@@ -2,10 +2,10 @@ import { crossfitBytomThrowdown } from "./events.js";
 
 export const media = [
   "hyroxcompetition.jpg",
-  "crossfit_runners.mp4",
-  "kettlebell_endurance.mp4",
-  "start_bytom.mp4",
-  // crossfitBytomThrowdown,
+  // "crossfit_runners.mp4",
+  // "kettlebell_endurance.mp4",
+  // "start_bytom.mp4",
+  crossfitBytomThrowdown,
 
 
 ];
