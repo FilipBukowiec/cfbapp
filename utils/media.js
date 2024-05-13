cf4501ebcce06393f0388c48da9b921887d481a7
FilipBukowiec <102471937+FilipBukowiec@users.noreply.github.com>
2024-05-13 11:57:14 +0200

    add runners

--- utils/media.js
+++ utils/media.js
@@ -4,6 +4,8 @@ export const media = [
   "https://drive.google.com/file/d/10zIbnx4_ccEpQ6ZdpzNBBvV4LL1cKIk-/preview",
   "https://i.gifer.com/Ae3y.gif",
   "hyroxcompetition.jpg",
+  "crossfit_runners.mp4",
+  "start_bytom.mp4",
   crossfitBytomThrowdown,
   "https://i.gifer.com/4A5.gif",
   "ad.mp4",
