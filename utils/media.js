ea7e33d1a86992971a7fb863d394bc233b8b9c34
FilipBukowiec <102471937+FilipBukowiec@users.noreply.github.com>
2024-05-14 18:01:55 +0200

    add media

--- utils/media.js
+++ utils/media.js
@@ -6,10 +6,6 @@ export const media = [
   "hyroxcompetition.jpg",
   "crossfit_runners.mp4",
   "kettlebell_endurance.mp4",
-  // "start_bytom.mp4",
-  // // crossfitBytomThrowdown,
-  // // new EventCountdown("2024-07-23T09:45:00", "murph.jpg");
-  // () => eventCountdown('murph.jpg')
-
-
+  "start_bytom.mp4",
+  "murph.jpg",
 ];
