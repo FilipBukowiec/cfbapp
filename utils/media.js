9b75b30ed94b53018109ba9d9b43d4707f4da61f
FilipBukowiec <102471937+FilipBukowiec@users.noreply.github.com>
2024-05-29 22:02:52 +0200

    update media

--- utils/media.js
+++ utils/media.js
@@ -5,11 +5,12 @@ import { eventCountdown } from "./events.js";
 export const media = [
   "wakacje.jpg",
   "intro.mp4",
-  "hyroxcompetition.jpg",
+  "murph.mp4",
   "hms.mp4",
   "crossfit_runners.mp4",
   "kettlebell_endurance.mp4",
-  "murph.mp4",
   "start_bytom.mp4",
-  "hyrox.mp4"
+  "power_woman.jpg",
+  "hyrox.mp4",
+  "hyroxcompetition.jpg",
 ];
