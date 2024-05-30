e375d97dc479c88fc2343989b3104d1ee2e26988
FilipBukowiec <102471937+FilipBukowiec@users.noreply.github.com>
2024-05-30 22:26:55 +0200

    change news

--- utils/news.js
+++ utils/news.js
@@ -1,12 +1,12 @@
 const textInfo = [
-  "15% kod rabatowy: CFBYTOM15 na www.mojcatering.com.pl",
-  "Start CrossFit Intro: 11/06 Wtorek (17:30)",
-  "Challenge Murph All Week: 10/06 -15/06",
+  "MÓJ CATERING nowym partnerem CrossFit Bytom. Wejdź na stronę mojcatering.com.pl i zyskaj 15% rabatu z kodem: CFBYTOM15",
+  "Start CrossFit Intro: 11/06 Wtorek godz. 17:30",
+  "Challenge Murph All Week: 10/06 - 15/06",
   "Lipiec: California 13/07 Sobota (10:00)",
   "Sierpień: Triple 3 17/08 Sobota (10:00) CrossFit Games Event na stadionie",
   "Crossfit Runners: #3 Wyjazd w Góry - 02/06 Niedziela (7:00)",
-  "Piotr Komaniecki startuje w European Masters Throwdown (Budapeszt) - Piątek 30/06",
-  "31/05-02/06 ostatnie eliminacje do CrossFit Games. Semifinals in RPA/Brazylia i USA. Oglądaj Live na crossfitgames.com",
+  "Nasz klubowicz Piotr Komaniecki startuje w European Masters Throwdown w Budapeszcie. Zawody odbęda się w piątek 30/06. Powodzenia!!!",
+  "31/05-02/06 ostatnie eliminacje do CrossFit Games. Semifinals in RPA/Brasil & USA. Oglądaj live na crossfitgames.com",
 ];
 
 let imgPath = "./img/cfb.svg";
