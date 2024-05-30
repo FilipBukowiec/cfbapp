a91c0562806c476cc1f3d343f4352c2dcdd7b15b
FilipBukowiec <102471937+FilipBukowiec@users.noreply.github.com>
2024-05-30 11:25:59 +0200

    update news

--- utils/news.js
+++ utils/news.js
@@ -1,11 +1,11 @@
 const textInfo = [
   "15% kod rabatowy : CFBYTOM15 na www.mojcatering.pl",
-  "Start CrossFit Intro: 11/06 Wtorek (17:30) ",
+  "Start CrossFit Intro: 11/06 Wtorek (17:30)",
   "Challenge Murph All Week: 10/06 -15/06",
   "Lipiec: California 13/07 Sobota (10:00)",
   "Sierpień: Triple 3 17/08 Sobota (10:00) CrossFit Games Event na stadionie",
   "Crossfit Runners: #3 Wyjazd w Góry - 02/06 Niedziela (7:00)",
-  "Piotr Komaniecki startuje w European Masters Throwdown (Budapeszt) - Piątek 31/06",
+  "Piotr Komaniecki startuje w European Masters Throwdown (Budapeszt) - Piątek 30/06",
   "31/05-02/06 ostatnie eliminacje do CrossFit Games. Semifinals in RPA/Brazylia i USA. Oglądaj Live na crossfitgames.com",
 ];
 
