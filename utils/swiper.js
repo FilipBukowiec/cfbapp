2bcdcbd5c79604ce5e52ca0f2bdd5f98a6657c4d
FilipBukowiec <102471937+FilipBukowiec@users.noreply.github.com>
2024-05-30 05:59:17 +0200

    change swipper

--- utils/swiper.js
+++ utils/swiper.js
@@ -6,85 +6,108 @@ export function mediaSlides(media) {
   // Wyczyść zawartość wrappera przed dodaniem nowych slajdów
   wrapper.innerHTML = '';
 
-  media.forEach((element) => {
-    const div = document.createElement("div");
-    div.classList.add("swiper-slide");
-    wrapper.appendChild(div);
+  const loadPromises = media.map((element) => {
+    return new Promise((resolve, reject) => {
+      const div = document.createElement("div");
+      div.classList.add("swiper-slide");
+      wrapper.appendChild(div);
 
-    if (element.includes("http")) {
-      if (element.includes(".mp4")) {
-        const videoElement = document.createElement("video");
-        videoElement.src = element;
-        videoElement.muted = true;
-        videoElement.setAttribute("playsinline", ""); // Ważne dla autoplay na mobilnych urządzeniach
-        videoElement.setAttribute("poster", "path/to/placeholder.jpg"); // Dodaj poster
-        videoElement.setAttribute("preload", "metadata");
-        div.appendChild(videoElement);
-      } else if (
-        element.includes(".jpg") ||
-        element.includes(".gif") ||
-        element.includes(".jpeg")
-      ) {
-        const imageElement = document.createElement("img");
-        imageElement.src = element;
-        div.appendChild(imageElement);
-      }
-    } else {
-      if (element.includes(".mp4")) {
-        const videoElement = document.createElement("video");
-        videoElement.src = `./media/${element}`;
-        videoElement.muted = true;
-        videoElement.setAttribute("playsinline", ""); // Ważne dla autoplay na mobilnych urządzeniach
-        videoElement.setAttribute("poster", "../media/bg.jpg"); // Dodaj poster
-        videoElement.setAttribute("preload", "metadata");
-        div.appendChild(videoElement);
-      } else if (
-        element.includes(".jpg") ||
-        element.includes(".gif") ||
-        element.includes(".jpeg")
-      ) {
-        const imageElement = document.createElement("img");
-        imageElement.src = `./media/${element}`;
-        div.appendChild(imageElement);
-      } else {
-        div.innerHTML = element;
-      }
-    }
-  });
+      if (element.includes("http")) {
+        if (element.includes(".mp4")) {
+          const videoElement = document.createElement("video");
+          videoElement.src = element;
+          videoElement.muted = true;
+          videoElement.setAttribute("playsinline", ""); // Ważne dla autoplay na mobilnych urządzeniach
+          videoElement.setAttribute("poster", "path/to/placeholder.jpg"); // Dodaj poster
+          videoElement.setAttribute("preload", "metadata");
 
-  const mySwiper = new Swiper(".swiper", {
-    loop: true,
-    autoplay: {
-      delay: 15000,
-      disableOnInteraction: false,
-    },
-    speed: 800,
-    effect: "fade",
-    fadeEffect: {
-      crossFade: true,
-    },
-    allowTouchMove: false,
-    on: {
-      slideChangeTransitionStart: function () {
-        const currentSlide = this.slides[this.activeIndex];
-        const video = currentSlide.querySelector("video");
+          videoElement.onloadeddata = resolve;
+          videoElement.onerror = reject;
 
-        if (video) {
-          video.currentTime = 0;
-          video.play();
+          div.appendChild(videoElement);
+        } else if (
+          element.includes(".jpg") ||
+          element.includes(".gif") ||
+          element.includes(".jpeg")
+        ) {
+          const imageElement = document.createElement("img");
+          imageElement.src = element;
 
-          const swiperInstance = this;
+          imageElement.onload = resolve;
+          imageElement.onerror = reject;
 
-          swiperInstance.autoplay.stop();
-
-          video.addEventListener("ended", function () {
-            swiperInstance.autoplay.start();
-            swiperInstance.slideNext();
-          }, { once: true }); // Dodaj { once: true }, aby nasłuchiwacz był wywołany tylko raz
+          div.appendChild(imageElement);
         }
-      },
-    },
+      } else {
+        if (element.includes(".mp4")) {
+          const videoElement = document.createElement("video");
+          videoElement.src = `./media/${element}`;
+          videoElement.muted = true;
+          videoElement.setAttribute("playsinline", ""); // Ważne dla autoplay na mobilnych urządzeniach
+          videoElement.setAttribute("poster", "../media/bg.jpg"); // Dodaj poster
+          videoElement.setAttribute("preload", "metadata");
+
+          videoElement.onloadeddata = resolve;
+          videoElement.onerror = reject;
+
+          div.appendChild(videoElement);
+        } else if (
+          element.includes(".jpg") ||
+          element.includes(".gif") ||
+          element.includes(".jpeg")
+        ) {
+          const imageElement = document.createElement("img");
+          imageElement.src = `./media/${element}`;
+
+          imageElement.onload = resolve;
+          imageElement.onerror = reject;
+
+          div.appendChild(imageElement);
+        } else {
+          div.innerHTML = element;
+          resolve();
+        }
+      }
+    });
   });
 
-  mySwiper.update();
+  Promise.all(loadPromises).then(() => {
+    const mySwiper = new Swiper(".swiper", {
+      loop: true,
+      autoplay: {
+        delay: 15000,
+        disableOnInteraction: false,
+      },
+      speed: 800,
+      effect: "fade",
+      fadeEffect: {
+        crossFade: true,
+      },
+      allowTouchMove: false,
+      on: {
+        slideChangeTransitionStart: function () {
+          const currentSlide = this.slides[this.activeIndex];
+          const video = currentSlide.querySelector("video");
+
+          if (video) {
+            video.currentTime = 0;
+            video.play();
+
+            const swiperInstance = this;
+
+            swiperInstance.autoplay.stop();
+
+            video.addEventListener("ended", function () {
+              swiperInstance.slideNext();
+              swiperInstance.autoplay.start();
+            }, { once: true }); // Dodaj { once: true }, aby nasłuchiwacz był wywołany tylko raz
+          }
+        },
+      },
+    });
+
+    mySwiper.update();
+  }).catch((error) => {
+    console.error("Błąd podczas ładowania mediów:", error);
+  });
 }
