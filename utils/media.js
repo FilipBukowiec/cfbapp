f973b6c642ad1f8a90da69cc875c884f57da7685
FilipBukowiec <102471937+FilipBukowiec@users.noreply.github.com>
2024-06-03 12:07:18 +0200

    change media

--- utils/media.js
+++ utils/media.js
@@ -5,8 +5,10 @@ import { eventCountdown } from "./events.js";
 export const media = [
   "wakacje.jpg",
   "intro.mp4",
+  "msp.jpg",
   "murph.mp4",
   "hms.mp4",
+  "best_fit.jpg",
   "crossfit_runners.mp4",
   "kettlebell_endurance.mp4",
   "start_bytom.mp4",
