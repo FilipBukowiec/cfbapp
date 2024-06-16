66eb187428bf4f48644dd99c1759aa420d5923f5
FilipBukowiec <102471937+FilipBukowiec@users.noreply.github.com>
2024-06-16 11:00:39 +0200

    change news

--- utils/news.js
+++ utils/news.js
@@ -3,11 +3,9 @@ const textInfo = [
   "Katarzyna Hendel zajmuje 3 miejsce w zawodach BestFit Games! Gratulujemy!!!",
   "Michał Szebestik zajmuje 5 miejsce w zawodach BestFit Games! Gratulujemy!!!",
   "Nasi klubowicze Gosia&Adam zajmują 7 miejsce w zawodach CROSS MIXED ZONE GDAŃSK 2024! GRATULUJEMY!!!",
-  "Nasza trenerka Monia wygrywa kategorię K-20 Dychy na Jeleni Skok podczas festiwalu Bieg Rzeźnika",
   "Młodzi sztangiści „Start Bytom” przywożą medale z indywidualnych Mistrzostw Śląska U20 & U23",
   "MÓJ CATERING nowym partnerem CrossFit Bytom. Wejdź na stronę mojcatering.com.pl i zyskaj 15% rabatu z kodem: CFBYTOM15",
-  "Start CrossFit Intro: 11/06 Wtorek godz. 17:30",
-  "Challenge Murph All Week: 10/06 - 15/06",
+  "CrossFit Intro: wtorek/czwartek godz. 17:30. Zapraszamy!!!",
   "Lipiec: California 13/07 Sobota (10:00)",
   "Sierpień: Triple 3 17/08 Sobota (10:00) CrossFit Games Event na stadionie",
 ];
