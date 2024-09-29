73809ed059eb35db464baf9d2eb41379895a404e
FilipBukowiec <102471937+FilipBukowiec@users.noreply.github.com>
2024-09-29 17:20:52 +0200

    change news

--- utils/news.js
+++ utils/news.js
@@ -1,4 +1,5 @@
 const textInfo = [
+  "Intro Kettlebell Endurance Zajęcia dla początkujących Trening Optymalny Start: Wtorek 08/10 , Godzina: 17:00",
   "Sukces naszych sztangistek w Ogólnopolskiej Olimpiadzie Młodzieży w podnoszeniu ciężarów! Wiktoria Kruk zajęła 6. miejsce w kategorii do 55 kg, zaś Oliwia Wittig uplasowała się na 4. miejscu w kategorii do 64 kg! Gratulujemy naszym zawodniczkom i życzymy dalszych sukcesów!!!",
   "MÓJ CATERING nowym partnerem CrossFit Bytom. Wejdź na stronę mojcatering.com.pl i zyskaj 15% rabatu z kodem: CFBYTOM15",
   "CrossFit Intro: wtorek/czwartek godz. 17:30. Zapraszamy!!!",
